e98641bfb820fb5bd3ecbecabd613bfe1efce509
JCruz6725 <john.cruz6725@gmail.com>
2024-09-06 09:49:09 -0500

    simple task

--- Program.cs
+++ Program.cs
@@ -1,7 +1,34 @@
 ﻿namespace ConsolePOC.ScheduledTask {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+			try {
+                Console.WriteLine("Starting task.");
+                int _ = ScheduledTaskRunner(5);      
+                Console.WriteLine("Task complete.");
+			}
+
+			catch(Exception) {
+                Task.Delay(1000).Wait();
+                Console.WriteLine("Error");
+			}
+
+            finally {
+                Task.Delay(1000).Wait();
+                Console.WriteLine("Task clean up."); 
+            }
+
+            Task.Delay(1000).Wait();
+            Console.WriteLine("Task End.");
+        }
+
+
+        static int ScheduledTaskRunner(int seconds) {
+            for(int i = 0; i < seconds; i++) { 
+                Task.Delay(1000).Wait(); 
+                Console.WriteLine(i.ToString());
+            }
+            return 0;         
         }
     }
 }
